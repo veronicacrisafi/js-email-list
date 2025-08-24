9daf1469a157f6840f206c4b404ef01cc59c07ec
veronicacrisafi <veronicacrisafi3@gmail.com>
2025-08-24 17:17:31 +0200

    eliminato file css sistemato tutto il codice in js e html

--- main.js
+++ main.js
@@ -1,19 +1,39 @@
-const api_url = "https://flynn.boolean.careers/exercises/api/random/mail";
+cconsole.log("Hello, Axios!");
 
-const mailList = document.getElementById("mail-list");
-
-console.log(api_url, mailList);
-
-// qui con la chiamata axios mi fa generare solo una mail
-axios.get(api_url).then((response) => {
-  //console.log(response);
-  const resultList = [response.data.response];
-  console.log(resultList);
-
-  mailList.innerHTML = resultList;
-
-  for (let i = 0; i < resultList.length; i++) {
-    const thisResList = [resultList[i]];
-    console.log(thisResList);
+document.addEventListener("DOMContentLoaded", function () {
+  const lista = document.getElementById("lista-email");
+  lista.style.listStyleType = "none";
+  const bottone = document.getElementById("genera-mail");
+  bottone.style.backgroundColor = "lightblue";
+  bottone.style.border = "none";
+  bottone.style.padding = "30px 100px";
+  bottone.style.borderRadius = "5px";
+  bottone.style.cursor = "pointer";
+  bottone.style.fontSize = "30px";
+  bottone.style.marginTop = "20px";
+  bottone.style.fontWeight = "bold";
+  bottone.style.color = "white";
+  function generaEmail() {
+    lista.innerHTML = "";
+    for (let i = 0; i < 10; i++) {
+      axios
+        .get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((response) => {
+          const email = response.data.response;
+          const li = document.createElement("li");
+          li.style.marginBottom = "10px";
+          li.style.border = "1px solid ";
+          li.style.borderRadius = "5px";
+          li.style.padding = "10px";
+          li.style.backgroundColor = "#f9f9f9";
+          li.textContent = email;
+          lista.appendChild(li);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
   }
+  generaEmail();
+  bottone.addEventListener("click", generaEmail);
 });
